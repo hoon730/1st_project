29ce9d12c3eaf10b10b9c28d92a8fe5f9b61b9fe
hoon730 <ehdgns730@gmail.com>
2024-07-02 18:22:19 +0900

    main_js

--- 1st_project/script.js
+++ 1st_project/script.js
@@ -1,28 +1,30 @@
-// const slide = document.querySelector(".main_left_text_slide");
-// const slideHeight = slide.offsetHeight;
-// console.log(slideHeight);
-// const wholeSlide = document.querySelectorAll(".main_left_text_slide_wrapper span");
-// const slideCount = wholeSlide.length;
-// const slideWrapper = document.querySelector(".main_left_text_slide_wrapper");
-// let slideIdex = 0;
+const slider = document.querySelector(".main_left_text_slide_wrapper_slider");
+const Slides = document.querySelectorAll(".main_left_text_slide_wrapper_slider span");
+const sliderHeight = slider.querySelector("span").offsetHeight;
 
-// const upSlide = () => {
-//   slideIdex++;
-//   slideWrapper.style.transform = `translateY(-${slideHeight}px)`;
-//   if(slideIdex > slideCount){
-//     slide.appendChild(wholeSlide);
-//     slideIdex = 0;
-//   }
-// };
+let slideCount = Slides.length;
+let currentIndex = 0;
+let sliderClone = slider.firstElementChild.cloneNode(true);
 
-// let startSlide = setInterval(upSlide, 3000);
+slider.appendChild(sliderClone);
+
+function startslide(){
+  currentIndex++;
+  slider.style.transition = "all 0.6s";
+  slider.style.transform = `translateY(${sliderHeight}*${currentIndex}px)`;
+
+  if(currentIndex == slideCount){
+    setTimeout(() => {
+      slider.style.transition = "0";
+      slider.style.transform = "translateY(0px)";
+    },700);
+    currentIndex = 0;
+  }
+}
+
+setInterval(startslide, 3000);
 
 window.addEventListener("scroll", function() {
-  let scrolling = this.screenY;
-
-  const main = this.document.getElementById("main");
-
-  if(main.offsetTop <= 0){
-    main.classList.add("active");
-  }
+  let windowScroll = this.scrollY;
+  
 });

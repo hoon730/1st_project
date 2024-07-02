8187c5dcbfc65018dbb939e654232401006395a1
Hoon <ehdgns730@gmail.com>
2024-07-02 23:41:06 +0900

    240702_mainjs

--- 1st_project/script.js
+++ 1st_project/script.js
@@ -24,6 +24,50 @@ function startslide(){
 
 setInterval(startslide, 3000);
 
+
+//main 모든 텍스트 이벤트 
+const mainLeftText1 = document.querySelector(".main_left_text_top span");
+const mainLeftText2 = document.querySelector(".main_left_text_middle span");
+let text1Container = mainLeftText1.textContent;
+let text2Container = mainLeftText2.textContent;
+mainLeftText1.innerHTML = "";
+mainLeftText2.innerHTML = "";
+
+
+window.onload = function() {
+  for(let char of text1Container) {
+    let span = document.createElement("span");
+    span.textContent = char;
+    mainLeftText1.appendChild(span);
+    mainLeftText1.style.transform = "translateY(100%)";
+
+  }
+  
+  for(let char of text2Container) {
+    let span = document.createElement("span");
+    span.textContent = char;
+    if(span.innerText === "Y" || span.innerHTML === "D"){
+      span.style.fontFamily = "Fugaz One"
+      span.style.textShadow = "-5px 0px var(--box-border-color), 0px 5px  var(--box-border-color), 1px 0px  var(--box-border-color), 0px -1px  var(--box-border-color)"
+    }
+    mainLeftText2.appendChild(span);
+    mainLeftText2.style.transform = "translateY(100%)";
+  }
+
+  mainLeftText1.querySelectorAll("span").forEach((text, index) => {
+    setTimeout(() => {
+      text.style.transform = "translateY(0)";
+    }, 80*(index+1));
+  })
+  
+  mainLeftText2.querySelectorAll("span").forEach((text, index) => {
+    setTimeout(() => {
+      text.style.transform = "translateY(0)";
+    }, 80*(index+1));
+  })
+};
+
+
 window.addEventListener("scroll", function() {
   let windowScroll = this.scrollY;
   
